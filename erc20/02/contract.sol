941fdeaee2ca5e86d29feef647e6455f7215b072
0x-stan <744428333@qq.com>
2024-10-10 11:45:50 +0800

    doc: update erc20

--- erc20/02/contract.sol
+++ erc20/02/contract.sol
@@ -2,27 +2,15 @@
 pragma solidity 0.8.21;
 
 contract Token {
-    uint256 public totalSupply;
-    string public name;
-    string public symbol;
-    uint8 public decimals;
-    mapping(address => uint256) public balanceOf;
-    // mapping allowance
+    // totalSupply
+    
+    // name, symbol, decimals
 
-    // Transfer event
+    // balanceOf
 
-    // Approval event;
+    // event Transfer
 
-    constructor(string memory _name, string memory _symbol, uint8 _decimals) {
-        name = _name;
-        symbol = _symbol;
-        decimals = _decimals;
+    constructor() {
     }
-
-    // function transfer
-
-    // function approve
-
-    // function transferFrom
 
 }
---
+++ erc20/03/contract.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.21;
+
+contract Token {
+    uint256 public totalSupply;
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+    mapping(address => uint256) public balanceOf;
+    // mapping allowance
+
+    // Transfer event
+
+    // Approval event;
+
+    constructor(string memory _name, string memory _symbol, uint8 _decimals) {
+        name = _name;
+        symbol = _symbol;
+        decimals = _decimals;
+    }
+
+    // function transfer
+
+    // function approve
+
+    // function transferFrom
+
+}
